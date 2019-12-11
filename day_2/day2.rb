8206b907feab76f6c0fea9d956956ddac218c223
Camille Bonnet <bonnet.cam@gmail.com>
2019-12-11 12:07:53 +0100

    Day 2 - DONE

---
+++ day_2/day2.rb
@@ -0,0 +1,18 @@
+class Day2
+  def self.process(opcode)
+    output_opcode = opcode
+    opcode.each_with_index do |value, index|
+      next unless index % 4 == 0
+      break if value == 99
+      index_1 = output_opcode[index + 1]
+      index_2 = output_opcode[index + 2]
+      index_3 = output_opcode[index + 3]
+      if value == 1
+        output_opcode[index_3] = output_opcode[index_1] + output_opcode[index_2]
+      else
+        output_opcode[index_3] = output_opcode[index_1] * output_opcode[index_2]
+      end
+    end
+    output_opcode
+  end
+end
