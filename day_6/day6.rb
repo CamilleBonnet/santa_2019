5b44077f72abf1a820022b0ab8ac3c747684bdc3
Camille Bonnet <bonnet.cam@gmail.com>
2019-12-13 21:32:29 +0100

    Refacto for day 6 after part 2

--- day_6/day6.rb
+++ day_6/day6.rb
@@ -1,50 +1,33 @@
 class Day6
   def self.process(input)
-    splitted_duos = input.dup.map{|v| v.split(")")}
-
-    centers  = splitted_duos.map{|duo| duo[0]}
-    in_orbit = splitted_duos.map{|duo| duo[1]}
-
-    orbit_count = {}
-
-    in_orbit.each_with_index do |planet, index|
-      i = index
-      orbit = planet
-      center = centers[i]
-
-      links = 1
-
-      while center != "COM"
-        links += 1
-        i = in_orbit.index(center)
-        center = centers[i]
-      end
-      orbit_count[planet] = links
-    end
-    orbit_count.values.sum
+    compute_path_to_COM(input)
+    @orbit_path.values.flatten.size
   end
 
   def self.compute_santa_path(input)
+    compute_path_to_COM(input)
+    meeting_point = (@orbit_path["SAN"] & @orbit_path["YOU"]).first
+    min_path = @orbit_path["SAN"].index(meeting_point) + @orbit_path["YOU"].index(meeting_point)
+  end
+
+  def self.compute_path_to_COM(input)
     splitted_duos = input.dup.map{|v| v.split(")")}
 
     centers  = splitted_duos.map{|duo| duo[0]}
     in_orbit = splitted_duos.map{|duo| duo[1]}
 
-    orbit_path = {}
+    @orbit_path = {}
 
     in_orbit.each_with_index do |planet, index|
       i = index
-      orbit = planet
       center = centers[i]
-      orbit_path[planet] = []
+      @orbit_path[planet] = [center]
 
       while center != "COM"
-        orbit_path[planet] << center
         i = in_orbit.index(center)
         center = centers[i]
+        @orbit_path[planet] << center
       end
     end
-    meeting_point = (orbit_path["SAN"] & orbit_path["YOU"]).first
-    min_path = orbit_path["SAN"].index(meeting_point) + orbit_path["YOU"].index(meeting_point)
   end
 end
--- day_6/day6_test.rb
+++ day_6/day6_test.rb
@@ -4,40 +4,14 @@ require 'pry'
 
 input = Main::DAY6
 
-test_value = [
-  "B)C",
-  "D)E",
-  "C)D",
-  "E)F",
-  "K)L",
-  "B)G",
-  "COM)B",
-  "E)J",
-  "G)H",
-  "D)I",
-  "J)K"
-]
-
-test_value_part_2 = [
-  "B)C",
-  "D)E",
-  "C)D",
-  "E)F",
-  "K)L",
-  "K)YOU",
-  "B)G",
-  "COM)B",
-  "I)SAN",
-  "E)J",
-  "G)H",
-  "D)I",
-  "J)K"
-]
+test_value_part_1 = ["B)C", "D)E", "C)D", "E)F", "K)L", "B)G", "COM)B", "E)J", "G)H", "D)I", "J)K"]
+test_value_part_2 = ["B)C", "D)E", "C)D", "E)F", "K)L", "K)YOU", "B)G", "COM)B", "I)SAN", "E)J", "G)H", "D)I", "J)K"]
 
 p "PART 1"
 p "Tests"
-test_result = Day6.process(test_value)
-p test_result == 42
+test_result_part_1 = Day6.process(test_value_part_1)
+p test_result_part_1 == 42
+p test_result_part_1
 
 p "Results"
 result = Day6.process(input)
@@ -50,6 +24,7 @@ p "Tests"
 test_result_part_2 = Day6.compute_santa_path(test_value_part_2)
 p 4 == test_result_part_2
 p test_result_part_2
+
 p "Results"
 r = Day6.compute_santa_path(input)
 p 340 == r
