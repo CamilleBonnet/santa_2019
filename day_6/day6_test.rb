9632c1c4a50e05fe6057082939fa73e6e8c9d535
Camille Bonnet <bonnet.cam@gmail.com>
2019-12-13 01:44:42 +0100

    Day 6 - DONE

--- day_6/day6_test.rb
+++ day_6/day6_test.rb
@@ -4,10 +4,53 @@ require 'pry'
 
 input = Main::DAY6
 
+test_value = [
+  "B)C",
+  "D)E",
+  "C)D",
+  "E)F",
+  "K)L",
+  "B)G",
+  "COM)B",
+  "E)J",
+  "G)H",
+  "D)I",
+  "J)K"
+]
+
+test_value_part_2 = [
+  "B)C",
+  "D)E",
+  "C)D",
+  "E)F",
+  "K)L",
+  "K)YOU",
+  "B)G",
+  "COM)B",
+  "I)SAN",
+  "E)J",
+  "G)H",
+  "D)I",
+  "J)K"
+]
+
 p "PART 1"
 p "Tests"
-result = Day6.process(input)
+test_result = Day6.process(test_value)
+p test_result == 42
 
 p "Results"
-# p 13210611 == result
+result = Day6.process(input)
+p 147223 == result
 p result
+
+p "----------"
+p "PART 2"
+p "Tests"
+test_result_part_2 = Day6.compute_santa_path(test_value_part_2)
+p 4 == test_result_part_2
+p test_result_part_2
+p "Results"
+r = Day6.compute_santa_path(input)
+p 340 == r
+p r
